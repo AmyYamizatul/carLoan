21d820c7c10e19f972f93708e9620257ac7badb6
AmyYamizatul <89080753+AmyYamizatul@users.noreply.github.com>
2021-10-06 21:30:13 +0800

    Update carLoan.py

--- carLoan.py
+++ carLoan.py
@@ -1,4 +1,22 @@
-st.header("Welcome to the RBA Car Loan Calculator")
+import streamlit as st 
+import numpy as np 
+import pandas as pd
+
+#import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import classification_report
+
+st.title("Welcome to the RBA Car Loan Calculator")
 
 while True:
 
